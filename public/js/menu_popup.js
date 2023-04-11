87a35fbe5aa14f42a12aa12a15d4261baf3831e7
Hu Guo <huhu8@ad.unc.edu>
2023-04-11 12:29:50 -0400

    update files for readability

--- public/js/menu_popup.js
+++ public/js/menu_popup.js
@@ -1,90 +1,116 @@
 
 document.addEventListener("DOMContentLoaded", function () {
-  // Get the menu button and the dropdown content
-  const menuButton = document.getElementById("menuButton");
-  const dropdownContent = document.querySelector(".dropdown-content");
-  const centerMainGarden = document.getElementById("centerMainGarden");
-  const centerLenoirGarden = document.getElementById("centerLenoir");
-  const centerGrahamGarden = document.getElementById("centerGraham");
-  const centerStacyGarden = document.getElementById("centerStacy");
-  const centerDavisGarden = document.getElementById("centerDavis");
-  const centerRamsHeadPlazaGarden = document.getElementById(
-    "centerRamsHeadPlaza"
-  );
-  const centerSASBPlazaGarden = document.getElementById("centerSASBPlaza");
-  const centerHardinGarden = document.getElementById("centerHardin");
-  const centerFetzerGarden = document.getElementById("centerFetzer");
+  // // Get the menu button and the dropdown content
+  // const menuButton = document.getElementById("menuButton");
+  // const dropdownContent = document.querySelector(".dropdown-content");
+  // const centerMainGarden = document.getElementById("centerMainGarden");
+  // const centerLenoirGarden = document.getElementById("centerLenoir");
+  // const centerGrahamGarden = document.getElementById("centerGraham");
+  // const centerStacyGarden = document.getElementById("centerStacy");
+  // const centerDavisGarden = document.getElementById("centerDavis");
+  // const centerRamsHeadPlazaGarden = document.getElementById(
+  //   "centerRamsHeadPlaza"
+  // );
+  // const centerSASBPlazaGarden = document.getElementById("centerSASBPlaza");
+  // const centerHardinGarden = document.getElementById("centerHardin");
+  // const centerFetzerGarden = document.getElementById("centerFetzer");
 
-  // Add an event listener for the menu button
-  menuButton.addEventListener("click", toggleDropdown);
+  // // Add an event listener for the menu button
+  // menuButton.addEventListener("click", toggleDropdown);
 
-  // Function to toggle the dropdown menu
-  function toggleDropdown(event) {
-    event.stopPropagation();
-    if (dropdownContent.style.display === "block") {
-      dropdownContent.style.display = "none";
-    } else {
-      dropdownContent.style.display = "block";
+  // // Function to toggle the dropdown menu
+  // function toggleDropdown(event) {
+  //   event.stopPropagation();
+  //   if (dropdownContent.style.display === "block") {
+  //     dropdownContent.style.display = "none";
+  //   } else {
+  //     dropdownContent.style.display = "block";
+  //   }
+  // }
+
+  // // Add an event listener for clicks outside the menu
+  // document.addEventListener("click", function (event) {
+  //   if (dropdownContent.style.display === "block") {
+  //     dropdownContent.style.display = "none";
+  //   }
+  // });
+
+  // centerMainGarden.addEventListener("click", function (event) {
+  //   centerOnGarden("main");
+  //   dropdownContent.style.display = "none";
+  // });
+
+  // centerLenoirGarden.addEventListener("click", function (event) {
+  //   centerOnGarden("lenoir");
+  //   dropdownContent.style.display = "none";
+  // });
+
+  // centerGrahamGarden.addEventListener("click", function (event) {
+  //   centerOnGarden("graham");
+  //   dropdownContent.style.display = "none";
+  // });
+
+  // centerStacyGarden.addEventListener("click", function (event) {
+  //   centerOnGarden("stacy");
+  //   dropdownContent.style.display = "none";
+  // });
+
+  // centerDavisGarden.addEventListener("click", function (event) {
+  //   // console.log("davis button clicked");
+  //   centerOnGarden("davis");
+  //   dropdownContent.style.display = "none";
+  // });
+
+  // centerRamsHeadPlazaGarden.addEventListener("click", function (event) {
+  //   // console.log("rams Plaza button clicked");
+  //   centerOnGarden("ramsHeadPlaza");
+  //   dropdownContent.style.display = "none";
+  // });
+
+  // centerSASBPlazaGarden.addEventListener("click", function (event) {
+  //   // console.log("SASB Plaza button clicked");
+  //   centerOnGarden("SASBPlaza");
+  //   dropdownContent.style.display = "none";
+  // });
+
+  // centerHardinGarden.addEventListener("click", function (event) {
+  //   centerOnGarden("hardin");
+  //   dropdownContent.style.display = "none";
+  // });
+
+  // centerFetzerGarden.addEventListener("click", function (event) {
+  //   centerOnGarden("fetzer");
+  //   dropdownContent.style.display = "none";
+  // });
+
+  
+    const menuButton = document.getElementById("menuButton");
+    const dropdownContent = document.querySelector(".dropdown-content");
+  
+    menuButton.addEventListener("click", toggleDropdown);
+  
+    function toggleDropdown(event) {
+      event.stopPropagation();
+      dropdownContent.style.display = dropdownContent.style.display === "block" ? "none" : "block";
     }
-  }
-
-  // Add an event listener for clicks outside the menu
-  document.addEventListener("click", function (event) {
-    if (dropdownContent.style.display === "block") {
-      dropdownContent.style.display = "none";
-    }
-  });
-
-  centerMainGarden.addEventListener("click", function (event) {
-    centerOnGarden("main");
-    dropdownContent.style.display = "none";
-  });
-
-  centerLenoirGarden.addEventListener("click", function (event) {
-    centerOnGarden("lenoir");
-    dropdownContent.style.display = "none";
-  });
-
-  centerGrahamGarden.addEventListener("click", function (event) {
-    centerOnGarden("graham");
-    dropdownContent.style.display = "none";
-  });
-
-  centerStacyGarden.addEventListener("click", function (event) {
-    centerOnGarden("stacy");
-    dropdownContent.style.display = "none";
-  });
-
-  centerDavisGarden.addEventListener("click", function (event) {
-    // console.log("davis button clicked");
-    centerOnGarden("davis");
-    dropdownContent.style.display = "none";
-  });
-
-  centerRamsHeadPlazaGarden.addEventListener("click", function (event) {
-    // console.log("rams Plaza button clicked");
-    centerOnGarden("ramsHeadPlaza");
-    dropdownContent.style.display = "none";
-  });
-
-  centerSASBPlazaGarden.addEventListener("click", function (event) {
-    // console.log("SASB Plaza button clicked");
-    centerOnGarden("SASBPlaza");
-    dropdownContent.style.display = "none";
-  });
-
-  centerHardinGarden.addEventListener("click", function (event) {
-    centerOnGarden("hardin");
-    dropdownContent.style.display = "none";
-  });
-
-  centerFetzerGarden.addEventListener("click", function (event) {
-    centerOnGarden("fetzer");
-    dropdownContent.style.display = "none";
-  });
-
+  
+    document.addEventListener("click", function (event) {
+      if (dropdownContent.style.display === "block") {
+        dropdownContent.style.display = "none";
+      }
+    });
+  
+    dropdownContent.addEventListener("click", function (event) {
+      const target = event.target;
+      if (target.classList.contains("garden-button")) {
+        const garden = target.getAttribute("data-garden");
+        centerOnGarden(garden);
+        dropdownContent.style.display = "none";
+      }
+    });
 
   function centerOnGarden(garden) {
+
     // let gardenCoords;
     // let gardenName;
     let gardenCoords, gardenElement, gardenName, gardenImageSrc, offset;
@@ -138,12 +164,14 @@ document.addEventListener("DOMContentLoaded", function () {
     const zoomOutLevel = currentZoom - 1;
     const zoomInLevel = 18;
 
+
     map
       .flyTo(map.getCenter(), zoomOutLevel, { animate: true, duration: 1 })
       .once("moveend", () => {
         map
           .flyTo(gardenCoords, zoomInLevel, { animate: true, duration: 0.5 })
           .once("moveend", () => {
+
             gardenElement.bindPopup(
               `<img src="${gardenImageSrc}" alt="${gardenName}" class="popup-image">
               <p class="popup-text">${gardenName}</p>
@@ -152,6 +180,9 @@ document.addEventListener("DOMContentLoaded", function () {
             );
 
             gardenElement.openPopup();
+
+            // Move the view a little down to show the full popup label
+            //  map.panBy([0, -100], { animate: true, duration: 0.5 });
 
             // Add event listener for the navigate button
             document
@@ -187,6 +218,7 @@ document.addEventListener("DOMContentLoaded", function () {
                 fitSelectedRoutes: true,
               }).addTo(map);
 
+              
               // Create a new Mapbox map
               // Define the routing control options
 
