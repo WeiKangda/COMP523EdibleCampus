3106ed90c0ec2064052dda4d9775f594bb6c327e
Hu Guo <86079744+HuGuo-UNC-Chapel-Hill@users.noreply.github.com>
2023-04-19 19:18:38 -0400

    update popup label

--- public/js/indexScript.js
+++ public/js/indexScript.js
@@ -1,3 +1,5 @@
+
+
 const uncCampusBounds = [
   [35.878870, -79.092428], // Southwest corner coordinates
   [35.953868, -79.003511]  // Northeast corner coordinates
@@ -22,10 +24,11 @@ let layer = new L.TileLayer(
   {
     attribution:
       'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a>',
-    maxZoom: 18,
+    maxZoom: 19,
   }
 );
 map.addLayer(layer);
+
 
 const graham = L.polygon([
   [35.913114, -79.047187],
@@ -34,8 +37,7 @@ const graham = L.polygon([
   [35.913149, -79.047073],
   [35.913105, -79.047094],
 ])
-  .addTo(map)
-  .bindPopup("Graham");
+  .addTo(map);
 
 const stacy = L.polygon([
   [35.912904, -79.045814],
@@ -44,8 +46,7 @@ const stacy = L.polygon([
   [35.912902, -79.045694],
   [35.912867, -79.045712],
 ])
-  .addTo(map)
-  .bindPopup("Stacy");
+  .addTo(map);
 
 const davis = L.polygon([
   [35.910711, -79.048374],
@@ -53,41 +54,15 @@ const davis = L.polygon([
   [35.910692, -79.048366],
   [35.910644, -79.048386],
 ])
-  .addTo(map)
-  .bindPopup("Davis");
-
-// const lenoir = L.polygon([
-//     [35.910369, -79.048509],
-//     [35.910386, -79.048453],
-//     [35.910356, -79.048434],
-//     [35.910337, -79.048490]
-// ]).addTo(map).bindPopup('lenoir');
-
-// const lenoir = L.polygon([
-//     [35.910369, -79.048509],
-//     [35.910386, -79.048453],
-//     [35.910356, -79.048434],
-//     [35.910337, -79.048490]
-// ]).addTo(map).bindPopup('lenoir');
+  .addTo(map);
 
 const lenoir = L.polygon([
-  [35.910275, -79.048656],
-  [35.910261, -79.048648],
-  [35.910281, -79.048594],
-  [35.910295, -79.048603],
-
-  [35.91031, -79.048563],
-  [35.910297, -79.048555],
-  [35.91033, -79.048512],
-  [35.910316, -79.048504],
-
-  [35.910337, -79.048489],
-  [35.910357, -79.0485],
-  [35.91036, -79.048475],
-  [35.910347, -79.048453],
+  [35.91027379845903,  -79.0486514081524],
+  [35.91032755341199,  -79.04850998318146],
+  [35.91031335870982,  -79.04850754506263],
+  [35.910263183220366, -79.04864409379591],
 ])
-  .addTo(map)
-  .bindPopup("Lenoir");
+  .addTo(map);
 
 const rams_head_plaza = L.polygon([
   [35.905647, -79.045832],
@@ -95,8 +70,7 @@ const rams_head_plaza = L.polygon([
   [35.905683, -79.045986],
   [35.9057, -79.045975],
 ])
-  .addTo(map)
-  .bindPopup("Rams Head Plaza");
+  .addTo(map);
 
 const SASB_plaza = L.polygon([
   [35.904384, -79.04485],
@@ -104,8 +78,7 @@ const SASB_plaza = L.polygon([
   [35.904367, -79.044742],
   [35.904359, -79.044761],
 ])
-  .addTo(map)
-  .bindPopup("SASB Plaza");
+  .addTo(map);
 
 const hardin = L.polygon([
   [35.903815, -79.046311],
@@ -113,8 +86,7 @@ const hardin = L.polygon([
   [35.903861, -79.046225],
   [35.903832, -79.046223],
 ])
-  .addTo(map)
-  .bindPopup("Hardin");
+  .addTo(map);
 
 const fetzer = L.polygon([
   [35.909124, -79.04647],
@@ -122,8 +94,7 @@ const fetzer = L.polygon([
   [35.909283, -79.04646],
   [35.909271, -79.046508],
 ])
-  .addTo(map)
-  .bindPopup("Fetzer");
+  .addTo(map);
 
 // Add a polygon for the main garden and a popup label for it
 const main_garden = L.polygon([
@@ -132,130 +103,9 @@ const main_garden = L.polygon([
   [35.91163, -79.047447],
   [35.911545, -79.047662],
 ])
-  .addTo(map)
-  .bindPopup("Main Garden.")
-  .openPopup();
-
-// // Add an event listener for the "Center on Fetzer" button
-// document.getElementById("centerFetzerBtn").addEventListener("click", centerOnFetzer);
-
-// function centerOnFetzer() {
-//     // Center the map on Fetzer's coordinates
-//     const fetzerCoords = [35.909124, -79.046470];
-//     map.setView(fetzerCoords, 18);
-
-//     // Open Fetzer's popup
-//     fetzer.openPopup();
-// }
-
-// Add an event listener for the "Center on Lenoir" button
-// document
-//   .getElementById("centerLenoirBtn")
-//   .addEventListener("click", centerOnLenoir);
-
-// function centerOnLenoir() {
-//   const lenoirCenter = [35.910299, -79.048571];
-//   map.setView(lenoirCenter, 17);
-
-//   lenoir.openPopup();
-// }
-
-// // Add an event listener for the "Center on Graham" button
-// document
-//   .getElementById("centerGrahamBtn")
-//   .addEventListener("click", centerOnGraham);
-
-// function centerOnGraham() {
-//   const grahamCenter = [35.91314, -79.047127];
-//   map.setView(grahamCenter, 17);
-
-//   graham.openPopup();
-// }
-
-// // Add an event listener for the "Center on Stacy" button
-// document
-//   .getElementById("centerStacyBtn")
-//   .addEventListener("click", centerOnStacy);
-
-// function centerOnStacy() {
-//   const stacyCenter = [35.912915, -79.045755];
-//   map.setView(stacyCenter, 17);
-
-//   stacy.openPopup();
-// }
-
-// // Add an event listener for the "Center on Davis" button
-// document
-//   .getElementById("centerDavisBtn")
-//   .addEventListener("click", centerOnDavis);
-
-// function centerOnDavis() {
-//   const davisCenter = [35.910692, -79.048377];
-//   map.setView(davisCenter, 17);
-
-//   davis.openPopup();
-// }
-
-// // Add an event listener for the "Center on Hardin" button
-// document
-//   .getElementById("centerHardinBtn")
-//   .addEventListener("click", centerOnHardin);
-
-// function centerOnHardin() {
-//   const hardinCenter = [35.90384, -79.046266];
-//   map.setView(hardinCenter, 17);
-
-//   hardin.openPopup();
-// }
-
-// // Add an event listener for the "Center on Rams Head Plaza" button
-// document
-//   .getElementById("centerRamsHeadPlazaBtn")
-//   .addEventListener("click", centerOnRamsHeadPlaza);
-
-// function centerOnRamsHeadPlaza() {
-//   const ramsHeadPlazaCenter = [35.905664, -79.045909];
-//   map.setView(ramsHeadPlazaCenter, 17);
-
-//   rams_head_plaza.openPopup();
-// }
-
-// // Add an event listener for the "Center on SASB Plaza" button
-// document
-//   .getElementById("centerSASBPlazaBtn")
-//   .addEventListener("click", centerOnSASBPlaza);
-
-// function centerOnSASBPlaza() {
-//   const sasbPlazaCenter = [35.90438, -79.04479];
-//   map.setView(sasbPlazaCenter, 17);
-
-//   SASB_plaza.openPopup();
-// }
-// function onMapdbClick(e) {
-//   var name = prompt("placed marker at: " + e.latlng);
-//   var marker = L.marker([e.latlng.lat, e.latlng.lng]).addTo(map);
-//   marker.bindPopup(name).openPopup();
-// }
-
-// map.on('dblclick', onMapdbClick);
-
-// main_garden.on("click", openPlant);
-
-// function openPlant() {
-//   //document.getElementById("about").style.backgroundColor = '#ff6347';
-//   console.log("openPlant function called!");
-//   document.getElementById("plantTab").style.width = "100%";
-//   document.getElementById("map").style.height = "0vh";
-// }
-
-// function closePlant() {
-//   document.getElementById("plantTab").style.width = "0%";
-//   document.getElementById("map").style.height = "80vh";
-// }
-
-
-// import { loadGardenContent } from "/gardenContent.js";
+  .addTo(map);
 
 
 
-// loadGardenContent();
+  
+ 
